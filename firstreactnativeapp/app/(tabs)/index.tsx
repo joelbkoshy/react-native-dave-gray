2dfe767e190c911620f095357a27d5e9b74b3596
joelbkoshy <joelbkoshy@gmail.com>
2025-02-13 22:37:21 +0530

    adding an ImageBG

--- firstreactnativeapp/app/(tabs)/index.tsx
+++ firstreactnativeapp/app/(tabs)/index.tsx
@@ -1,25 +1,40 @@
-import {View,Text,StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import randomBg from '../../assets/images/landscape.jpg'
 
-const index = ()=>{
+const index = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
+      <ImageBackground
+        source={randomBg}
+        style={styles.image}
+      >
+        <Text style={styles.text}>Hello</Text>
+      </ImageBackground>
     </View>
   )
 
 }
 
-const styles  = StyleSheet.create({
-  container : {
-    flex:1,
-    flexDirection : "column",
-    justifyContent:'center'
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: 'center'
   },
-  text:{
-    color:"white",
-    fontSize:42,
-    fontWeight:"bold",
-    textAlign:"center"
+  image: {
+    width: "100%",
+    height:"100%",
+    flex:1,
+    justifyContent:"center"
+  },
+
+
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor:"rgba(0,0,0,0.5)"
   }
 })
 
